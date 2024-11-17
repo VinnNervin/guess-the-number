fcb80dc1115d65f5a503b91d497561509d523047
VinnNervin <lordcharles911@gmail.com>
2024-11-17 14:42:21 +0700

    "update and add js"

--- js/utility.js
+++ js/utility.js
@@ -1,14 +1,12 @@
 const hexLetter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 const buttonDiffDiv = document.querySelector('.list-diff')
 
-
 const difficultList = {
     easy: '⭐',
     medium: '😎',
     hard: '🥵',
     extreme: '💀'
 }
-
 let color = '#'
 for (let i = 0; i < 6; i++) {
     let random = Math.floor(Math.random() * 15)
@@ -23,7 +21,6 @@ for (const item in difficultList) {
     buttonDiffDiv.appendChild(createBtn).innerText = item + ' ' + difficultList[item]
     console.log(item);
 }
-
 const button2 = document.querySelectorAll('.list-diff button');
 button2.forEach((button) => {
     button.addEventListener('click', () => {
@@ -40,7 +37,6 @@ cancelBtn.classList.add('cancel')
 
 
 buttonDiffDiv.appendChild(cancelBtn).innerText = 'cancel'
-
 diffBtn.addEventListener('click', () => {
     diffMenu.style.visibility = 'visible'
 })
